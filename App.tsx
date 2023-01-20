045cbd4def006579f5ec6d48f3d2bf9a7d2ee247
Lucas <lucasefftingn@hotmail.com>
2023-01-20 18:29:33 -0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+export default function App(): React.ReactElement {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
     </View>
   );
 }
@@ -13,8 +12,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#fdfcfe",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
   },
 });
